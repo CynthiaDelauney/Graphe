1840384c966539e162e5ff40067e0a07bb8a4f8f
Cynthia Delauney <c.delauney@hotmail.fr>
2013-10-12 20:48:24 +0200

    ajout de la structure de file

--- graphe.ml
+++ graphe.ml
@@ -1,51 +1,112 @@
 
 
-(*
-    val tab_VNV : bool array 
-    le true signifie que le sommet est non visité, le false qu'il est visité,
-    par defaut tous les sommets sont non visités.
-*)
-let init_tab_VNV () = (Array.make 13 true) ;;
+(* =================================================================== *)
 
-(* val visite : int -> 'a array -> 'a = <fun> *)
-let visite = function sommet -> function tab_VNV -> tab_VNV.(sommet) ;;
+(* STRUCTURE DE FILE *)
+
+type 'a elem = { mutable prev : 'a _elem ;
+                 mutable next : 'a _elem ;
+                 info : 'a               }
+and 'a _elem =
+    | FVide
+    | Elem of 'a elem ;;
+
+type 'a _file = { mutable premier : 'a _elem ;
+                  mutable dernier : 'a _elem  } ;;
+
+let init_file () = {premier = FVide ; dernier = FVide} ;;
 
 (* 
-    val a_voisin : int -> int list array -> bool array -> bool = <fun> 
-    retourne vrai si le sommet x a au moins un voisin non visite
+    val insere_file : 'a -> 'a file -> unit = <fun> 
+    insere v dans la file
 *)
-let a_voisin = function x -> function graphe_l -> function tab_VNV ->
-    let liste_sommets_adj = graphe_l.(x) in
-        (*
-            'aux' parcourt la liste, et dit si cette liste contient au moins un sommet non visite 
-        *)
-        let rec aux = function liste -> function tab_VNV ->
-            if liste = []
-            then false
-            else 
-                ( if (visite (List.hd liste) tab_VNV)
-                  then true
-                  else (aux (List.tl liste) tab_VNV) )
-        in
-        (aux liste_sommets_adj tab_VNV) ;;
+let insere_file v f =
+    match f.premier with
+    | FVide -> let rec elem = Elem {prev = FVide ; next = FVide ; info = v} in
+                    f.premier <- elem ; f.dernier <- elem 
+    | _     -> let ndernier = Elem {prev = f.dernier ; next = FVide ; info = v} in
+               match f.dernier with
+               | FVide  -> failwith "problème, file.premier <> Fvide alors que file.dernier = FVide\n"
+               | Elem x -> x.next <- ndernier ; f.dernier <- ndernier ;;
 
-(* val init_file : unit -> 'a list = <fun> *)
-let init_file () = [] ;;
+(* 
+    'a _file -> unit = <fun> 
+    retire de la file l'element en tete de file
+*)
+let enleve_tete_file f =
+    match f.premier with
+    | FVide -> failwith "La file est vide, rien a faire\n"
+    | Elem x -> let npremier = x.next in
+                match npremier with
+                | FVide -> f.premier <- FVide ; f.dernier <- FVide
+                | Elem y -> let _ = y.prev <- FVide in
+                            f.premier <- npremier ;; 
 
-(* val affiche_list : int list -> unit = <fun> *)
-let affiche_list = function liste ->
-    let _ = Printf.printf "[" in
-        let rec aux = function
-            | []    -> Printf.printf "[]\n"
-            | a::[] -> Printf.printf "%d]\n" a
-            | a::q  -> let _ = (Printf.printf "%d;" a) in (aux q) 
-        in 
-        (aux liste) ;;
+(* 
+    val retirer_elem_file : 'a -> 'a _file -> unit = <fun> 
+    retire l'element elem de la file
+*)
+let rec retirer_elem_file a file elem =
+    match elem with 
+    | FVide -> failwith "La file est vide, rien a faire (1)\n"
+    | Elem x -> ( let rec aux = function
+                        | FVide -> failwith "La file est vide, rien a faire (2)\n"
+                        | Elem x -> if x.info = a
+                                    then (  match x.prev, x.next with
+                                            | FVide, FVide -> file.premier <- FVide ; file.dernier <- FVide
+                                            | FVide, (Elem suivant_x) -> file.premier <- x.next ; suivant_x.prev <- FVide
+                                            | (Elem precedent_x), FVide -> file.dernier <- x.prev ; precedent_x.next <- FVide
+                                            | (Elem precedent_x), (Elem suivant_x) -> let _ = precedent_x.next <- x.next in
+                                                                    suivant_x.prev <- x.prev ) 
+                                    else (aux x.next) 
+                    in 
+                        (aux elem)) ;;
+
+(*
+    val get_sommet_file : 'a _file -> 'a = <fun>
+    renvoie l'élément en tête de file'
+*)
+let get_sommet_file f =
+    match f.premier with
+    | FVide  -> failwith "La file est vide, rien a faire (3)\n"
+    | Elem x -> x.info ;; 
+
+(* =================================================================== *)
+
+(* STRUCTURE DE PILE *)
+
+let init_pile () = [] 
+
+(* 
+    val insere_pile : 'a -> 'a list -> 'a list = <fun> 
+    insere elem au debut de la liste
+*)
+let insere_pile elem pile = elem::pile
+
+(*
+    val enleve_tete_liste : 'a list -> 'a list = <fun>
+    enleve le premier element de la liste
+*)
+let enleve_tete_liste = function
+    | []   -> failwith "La pile est vide, rien à faire (4)\n"
+    | a::q -> q
+
+(*  
+    val get_sommet_pile : 'a list -> 'a = <fun>
+    retourne le sommet de la pile, premier elem de la liste
+*)
+let get_sommet_pile = function
+    | []   -> failwith "La pile est vide, rien à faire (5)\n"
+    | a::q -> a
+
+(* =================================================================== *)
+
+(* FONCTIONS D'AFFICHAGE *)
 
 (* 
     val affiche_tab : bool array -> unit = <fun>
 *)
-let affiche_tab = function tab ->
+let affiche_tab tab =
     match (Array.length tab) with 
     | 0 -> Printf.printf "[||]\n" 
     | 1 -> Printf.printf "[|%B|]\n" tab.(0)
@@ -55,70 +116,163 @@ let affiche_tab = function tab ->
                 done ;
                 Printf.printf "%B|]\n" tab.(t - 1) ;;
 
-(* 
-    val insere_file : 'a -> 'a list -> 'a list = <fun> 
-    insere elem a la fin de la liste
+(* val affiche_list : int list -> unit = <fun> *)
+let affiche_list liste =
+    let _ = Printf.printf "[" in
+        let rec aux = function
+            | []    -> Printf.printf "[]\n"
+            | a::[] -> Printf.printf "%d]\n" a
+            | a::q  -> let _ = (Printf.printf "%d;" a) in (aux q) 
+        in 
+            (aux liste) ;;
+
+let affiche_file file =
+    match file.premier with
+    | FVide  -> print_string "[]\n"
+    | Elem x -> (   let _ = print_string "[" in
+                    let rec aux = function
+                      | FVide -> Printf.printf "\b]\n"
+                      | Elem y -> 
+                        let _ = Printf.printf "%d " y.info  in 
+                                    (aux y.next)
+                      in (aux file.premier) ) ;; 
+
+(* =================================================================== *)
+
+(* UTILITAIRE *)
+
+(*
+    val tab_VNV : bool array 
+    le true signifie que le sommet est non visité, le false qu'il est visité,
+    par defaut tous les sommets sont non visités.
 *)
-let rec insere_file = function elem -> function 
-    | []    -> [elem]    
-    | a::q  -> a::(insere_file elem q) ;;
+let init_tab_VNV () = (Array.make 13 true) 
+
+
+(* val non_visite : int -> 'a array -> 'a = <fun> *)
+let non_visite sommet tab_VNV = tab_VNV.(sommet) ;;
 
 (* 
-    val enleve_tete : 'a list -> 'a list = <fun> 
-    enleve le premier element de la liste
+    val a_voisin : int -> int list array -> bool array -> bool = <fun> 
+    retourne vrai si le sommet x a au moins un voisin non visite
 *)
-let enleve_tete = function
-    | []    -> failwith "La file est vide, rien a faire\n"
-    | a::q  -> q ;;
-
-(* 
-    val maj_file : int list -> bool array -> int list array -> int list = <fun> 
-    Met a jour la file d'attente, les sommets fermes sont retires
-*)
-let rec maj_file = function tab_VNV -> function graphe_l -> function
-    | [] -> []
-    | a::q -> if (a_voisin a graphe_l tab_VNV) 
-              then a::(maj_file tab_VNV graphe_l q)
-              else begin tab_VNV.(a) <- false ; (maj_file tab_VNV graphe_l q) end ;;
+let a_voisin x graphe_l tab_VNV =
+    let liste_sommets_adj = graphe_l.(x) in
+        (* 'aux' parcourt la liste, et dit si cette liste contient au moins un sommet non visite *)
+        let rec aux liste tab_VNV = 
+            if liste = []
+            then false
+            else 
+                ( if (non_visite (List.hd liste) tab_VNV)
+                  then true
+                  else (aux (List.tl liste) tab_VNV) )
+        in
+            (aux liste_sommets_adj tab_VNV) ;;
 
 (*
     val choisir_voisin_nonvisite : int -> int list array -> bool array -> int = <fun>
     hyp : sommet a un voisin non visite
     renvoie le premier sommet non visite de la liste
 *)
-let choisir_voisin_nonvisite = function sommet -> function graphe_l -> function tab_VNV ->
+let choisir_voisin_nonvisite sommet graphe_l tab_VNV =
     let liste = graphe_l.(sommet) in
         let rec aux = function liste -> function tab_VNV ->
             if liste = [] 
-            then failwith "Liste sans sommets visites\n"
-            else (match (visite (List.hd liste) tab_VNV) with
+            then -1
+            else (match (non_visite (List.hd liste) tab_VNV) with
                   | true -> (List.hd liste)
-                  | _ -> (aux (List.tl liste) tab_VNV))
+                  | _    -> (aux (List.tl liste) tab_VNV))
         in
-        (aux liste tab_VNV) ;; 
+            (aux liste tab_VNV) ;; 
 
-let rec choisir_sommet_NV = function tab_VNV -> function graphe_l -> function
-    | t     when t = (Array.length graphe_l) -> - 1
-    | i     -> if (visite i tab_VNV) then i else (choisir_sommet_NV tab_VNV graphe_l (i + 1)) ;;
+(*
+    on parcourt tout les sommets, jusqu'a en trouver un qui soit non visité 
+*)
+let rec choisir_sommet_NV tab_VNV graphe_l sommet =
+    if sommet = (Array.length graphe_l) then -1
+    else (  if (non_visite sommet tab_VNV) 
+            then sommet 
+            else (choisir_sommet_NV tab_VNV graphe_l (sommet + 1)) ) ;;
+
+(* 
+    vam maj_pile : bool array -> int list array -> int list -> int list = <fun>
+    Met a jour la pile, les sommets fermes sont retires
+*)
+let rec maj_pile tab_VNV graphe_l = function
+    | [] -> []
+    | a::q -> if (a_voisin a graphe_l tab_VNV) 
+              then a::(maj_pile tab_VNV graphe_l q)
+              else begin 
+                   tab_VNV.(a) <- false ; 
+                   (maj_pile tab_VNV graphe_l q) 
+                   end ;;
+
+(*
+    val maj_file : bool array -> int list array -> int _file -> 'a list = <fun> 
+    Met a jour la file, les sommets fermes sont retires
+*)
+let rec maj_file tab_VNV graphe_l file elem = 
+    match elem with
+    | FVide -> ()
+    | Elem x -> let a = x.info in
+        if (a_voisin a graphe_l tab_VNV)
+        then (maj_file tab_VNV graphe_l file x.next)
+        else 
+            begin
+                tab_VNV.(a) <- false ;
+                (retirer_elem_file a file elem) ;
+                (maj_file tab_VNV graphe_l file x.next)
+            end ;;
 
 
-let rec parcours_largeur = function graphe_l -> function file_sommets -> function liste_parcours -> function tab_VNV ->
-    if file_sommets = [] 
+(* =================================================================== *)
+
+(* FONCTIONS DE PARCOURS *)
+
+(* int list array -> int list -> int list -> bool array -> int list = <fun> *)
+let rec parcours_largeur graphe_l file_sommets liste_parcours tab_VNV =
+    if file_sommets.premier = FVide 
     then ( match (choisir_sommet_NV tab_VNV graphe_l 0) with
            | -1     -> liste_parcours
-           | sommet -> (parcours_largeur graphe_l [sommet] (insere_file sommet liste_parcours) tab_VNV) )
+           | sommet ->  let _ = tab_VNV.(sommet) <- false in 
+                        let f = (init_file ()) in 
+                        let _ = (insere_file sommet f) in
+                        let _ = (insere_file sommet liste_parcours) in
+                            (parcours_largeur graphe_l f liste_parcours tab_VNV) )
     else
-        let premier_sommet_file = (List.hd file_sommets) in
+        let premier_sommet_file = (get_sommet_file file_sommets) in
             let x = (choisir_voisin_nonvisite premier_sommet_file graphe_l tab_VNV) in
                 begin
                 tab_VNV.(x) <- false ;
-                let nliste_parcours = (insere_file x liste_parcours) in
-                    let nfile_sommets = (maj_file tab_VNV graphe_l (insere_file x file_sommets)) in
-                        (parcours_largeur graphe_l nfile_sommets nliste_parcours tab_VNV) ;
+                let _ = (insere_file x liste_parcours) in
+                let _ = (insere_file x file_sommets) in
+                    let _ = (maj_file tab_VNV graphe_l file_sommets file_sommets.premier) in
+                        (parcours_largeur graphe_l file_sommets liste_parcours tab_VNV) ;
                 end ;; 
 
+(* int list array -> int list -> int list -> bool array -> int list = <fun> *)
+let rec parcours_profondeur graphe_l pile_sommets liste_parcours tab_VNV =
+    if pile_sommets = []
+    then ( match (choisir_sommet_NV tab_VNV graphe_l 0) with
+           | -1     -> liste_parcours
+           | sommet -> let _ = (insere_file sommet liste_parcours) in 
+                            (parcours_profondeur graphe_l [sommet] liste_parcours tab_VNV) )
+    else
+        let premier_sommet_pile = (get_sommet_pile pile_sommets) in 
+            let x = (choisir_voisin_nonvisite premier_sommet_pile graphe_l tab_VNV) in
+                if x = -1 then liste_parcours
+            else
+                begin
+                tab_VNV.(x) <- false ;
+                tab_VNV.(premier_sommet_pile) <- false ;
+                let _ = (insere_file x liste_parcours) in
+                    let npile_sommets = (maj_pile tab_VNV graphe_l (insere_pile x pile_sommets)) in
+                        (parcours_profondeur graphe_l npile_sommets liste_parcours tab_VNV) ;
+                end ;; 
 
-let main =
+(* =================================================================== *)
+
+let () =
 
     (*
         val graphe_l : int list array 
@@ -140,9 +294,14 @@ let main =
             (* 12 *) [11]                 ;
                    |]
     in
-
-        let tab_VNV = init_tab_VNV () in            (* initialise le tableau de bool à true *)
-        let liste_parcours = (init_file ()) in      (* liste vide *)
-        let file_sommets = (init_file ()) in        (* [] *)
-            (affiche_list (parcours_largeur graphe_l file_sommets liste_parcours tab_VNV)) ;;
+        let tab_VNV = init_tab_VNV () in          
+        let file_sommets = (init_file ()) in 
+        let pile_sommets = (init_pile ()) in        
+            begin
+            Printf.printf "\nParcours en largeur : " ;
+            (affiche_file (parcours_largeur graphe_l file_sommets (init_file ()) tab_VNV)) ;
+            Printf.printf "Parcours en profondeur : " ;
+            (affiche_file (parcours_profondeur graphe_l pile_sommets (init_file ()) (init_tab_VNV ()))) ;
+            print_string "\n"
+            end ;;
 
